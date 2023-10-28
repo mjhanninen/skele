af4fc03a408729f4c0a20da68450196f1eeefe25
Matti Hänninen <matti@mjhanninen.com>
2023-10-28 16:11:05 +0300

    Format code

--- rustybones/src/lib.rs
+++ rustybones/src/lib.rs
@@ -30,10 +30,10 @@ pub struct KeyIter<'a> {
     hasher: Sha256,
 }
 
-const FINGERPRINT_SALT: [u8; 32] = [0x15, 0x01, 0x8a, 0x3e, 0x8e, 0x79, 0x28, 0x71, 0x43, 0x70,
-                                    0xf7, 0x51, 0x1c, 0x3b, 0xd5, 0xce, 0x85, 0x2b, 0x6f, 0x91,
-                                    0x52, 0xc2, 0xb9, 0xab, 0xdb, 0x99, 0xaa, 0xd7, 0x9f, 0xc5,
-                                    0x51, 0x20];
+const FINGERPRINT_SALT: [u8; 32] = [
+    0x15, 0x01, 0x8a, 0x3e, 0x8e, 0x79, 0x28, 0x71, 0x43, 0x70, 0xf7, 0x51, 0x1c, 0x3b, 0xd5, 0xce,
+    0x85, 0x2b, 0x6f, 0x91, 0x52, 0xc2, 0xb9, 0xab, 0xdb, 0x99, 0xaa, 0xd7, 0x9f, 0xc5, 0x51, 0x20,
+];
 
 impl KeySource {
     pub fn new(key: &str) -> Self {
@@ -65,7 +65,7 @@ impl Drop for KeySource {
 impl<'a> KeyIter<'a> {
     pub fn new(sk: &'a [u8; 32], domain: &str, identity: &str) -> Self {
         let mut key_iter = KeyIter {
-            sk: sk,
+            sk,
             hash: [0u8; 32],
             hasher: Sha256::new(),
         };
@@ -113,19 +113,18 @@ pub mod base32 {
         8 * ((input_size + 4) / 5)
     }
 
-	#[allow(unused_parens)]
-	#[cfg_attr(rustfmt, rustfmt_skip)]
+    #[allow(unused_parens)]
     fn encode_block(q: &[u8], r: &mut [u8]) {
         assert!(q.len() == 5);
         assert!(r.len() == 8);
-		r[0] =  (q[0] >> 3);
-		r[1] = ((q[0] & 0b00000111) << 2) + (q[1] >> 6);
-		r[2] =  (q[1] & 0b00111110) >> 1;
-		r[3] = ((q[1] & 0b00000001) << 4) + (q[2] >> 4);
-		r[4] = ((q[2] & 0b00001111) << 1) + (q[3] >> 7);
-		r[5] =  (q[3] & 0b01111100) >> 2;
-		r[6] = ((q[3] & 0b00000011) << 3) + (q[4] >> 5);
-		r[7] =  (q[4] & 0b00011111);
+        r[0] = (q[0] >> 3);
+        r[1] = ((q[0] & 0b00000111) << 2) + (q[1] >> 6);
+        r[2] = (q[1] & 0b00111110) >> 1;
+        r[3] = ((q[1] & 0b00000001) << 4) + (q[2] >> 4);
+        r[4] = ((q[2] & 0b00001111) << 1) + (q[3] >> 7);
+        r[5] = (q[3] & 0b01111100) >> 2;
+        r[6] = ((q[3] & 0b00000011) << 3) + (q[4] >> 5);
+        r[7] = (q[4] & 0b00011111);
     }
 
     pub fn encode(input: &[u8], output: &mut [u8]) {
@@ -138,10 +137,11 @@ pub mod base32 {
     }
 
     // abcdefghjkmnpqrstvwxyz
-    const CROCKFORD_TRANSLATION_TABLE: [u8; 32] = [0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
-                                                   0x38, 0x39, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66,
-                                                   0x67, 0x68, 0x6a, 0x6b, 0x6d, 0x6e, 0x70, 0x71,
-                                                   0x72, 0x73, 0x74, 0x76, 0x77, 0x78, 0x79, 0x7a];
+    const CROCKFORD_TRANSLATION_TABLE: [u8; 32] = [
+        0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x61, 0x62, 0x63, 0x64, 0x65,
+        0x66, 0x67, 0x68, 0x6a, 0x6b, 0x6d, 0x6e, 0x70, 0x71, 0x72, 0x73, 0x74, 0x76, 0x77, 0x78,
+        0x79, 0x7a,
+    ];
 
     pub fn crockford(input: &[u8], output: &mut [u8]) {
         assert!(input.len() == output.len());
@@ -171,9 +171,9 @@ pub struct SplitGroups<'a> {
 pub fn split_groups(string: &str, group_size: usize) -> SplitGroups {
     if string.len() > 0 && group_size > 0 {
         SplitGroups {
-            string: string,
+            string,
             start: 0,
-            group_size: group_size,
+            group_size,
             finished: false,
         }
     } else {
@@ -220,7 +220,10 @@ pub fn upcase_first(string: &str) -> String {
 }
 
 pub fn to_canonical(input: &str) -> String {
-    split_groups(input, 4).map(upcase_first).collect::<Vec<String>>().join("-")
+    split_groups(input, 4)
+        .map(upcase_first)
+        .collect::<Vec<String>>()
+        .join("-")
 }
 
 pub fn format_key(key: &[u8], group_size: usize) -> String {
@@ -229,25 +232,28 @@ pub fn format_key(key: &[u8], group_size: usize) -> String {
     let mut b32_key = vec![0u8; n];
     base32::encode(&key, &mut b32_key);
     let key_str = base32::to_crockford(&b32_key);
-    let key_str =
-        split_groups(&key_str, group_size).map(upcase_first).collect::<Vec<String>>().join("-");
+    let key_str = split_groups(&key_str, group_size)
+        .map(upcase_first)
+        .collect::<Vec<String>>()
+        .join("-");
     key_str
 }
 
 #[cfg(test)]
 mod tests {
 
-	#[cfg_attr(rustfmt, rustfmt_skip)]
-    use ::{prepare_skeleton_key, PREPARED_SKELETON_KEY_SIZE, KeySource, KeyIter, base32, to_canonical};
+    use super::*;
 
     #[test]
     fn it_works() {
         let mut key = [0u8; PREPARED_SKELETON_KEY_SIZE];
         let rc = prepare_skeleton_key("\"Mom\" is \"äiti\" in Finnish", &mut key);
         assert!(rc == Ok(()));
-        let correct = [0x17, 0xa8, 0xea, 0x56, 0x34, 0xad, 0x5e, 0x06, 0x0f, 0xac, 0xab, 0x88,
-                       0x44, 0x3a, 0xf9, 0x81, 0x1d, 0x5d, 0x5a, 0x94, 0x77, 0x42, 0x7f, 0x53,
-                       0x3d, 0xa9, 0x93, 0xdb, 0x82, 0xad, 0x93, 0x4c];
+        let correct = [
+            0x17, 0xa8, 0xea, 0x56, 0x34, 0xad, 0x5e, 0x06, 0x0f, 0xac, 0xab, 0x88, 0x44, 0x3a,
+            0xf9, 0x81, 0x1d, 0x5d, 0x5a, 0x94, 0x77, 0x42, 0x7f, 0x53, 0x3d, 0xa9, 0x93, 0xdb,
+            0x82, 0xad, 0x93, 0x4c,
+        ];
         assert!(key == correct);
     }
 
